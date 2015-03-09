de5a2e640f88decaa832978175053e00483be21d
unknown <faktiss@jjj>
2015-03-09 22:39:59 +0100

    Exercice numero3: problème acces attributs onSuccess

--- src/main/java/com/talentroc/t5/interview/pages/contact/ContactEdit.java
+++ src/main/java/com/talentroc/t5/interview/pages/contact/ContactEdit.java
@@ -1,12 +1,13 @@
 package com.talentroc.t5.interview.pages.contact;
 
 
+import org.apache.tapestry5.annotations.Property;
+import org.apache.tapestry5.ioc.annotations.Inject;
+import org.apache.tapestry5.jpa.annotations.CommitAfter;
+
 import com.talentroc.t5.interview.entities.Contact;
 import com.talentroc.t5.interview.services.ContactManager;
 import com.talentroc.t5.interview.utils.BusinessException;
-
-import org.apache.tapestry5.annotations.Property;
-import org.apache.tapestry5.ioc.annotations.Inject;
 
 public class ContactEdit {
 
@@ -15,6 +16,8 @@ public class ContactEdit {
 	
     @Property
     private Contact contact;
+    
+    private Long id;
 
     void onActivate() {
         contact = new Contact();
@@ -22,17 +25,22 @@ public class ContactEdit {
 
     Boolean onActivate(Contact contact) {
         this.contact = contact;
+        this.id = contact.getId();
+       // System.out.println(this.id);
         return Boolean.TRUE;
     }
     
-    
+  
     Object onSuccess() throws BusinessException{
+    	//Problème : contact.getId() => null
+    	//			this.id ==> null
+    	//System.out.println(this.id);
     	try {
-    		
-    		contactManager.update(contact);
-		
-    	} catch (BusinessException e) {
     		contactManager.create(contact);
+    			//contactManager.update(contact);
+
+    	} catch (BusinessException e) {
+    		
 		e.printStackTrace();
     	}
 		return ContactIndex.class;
